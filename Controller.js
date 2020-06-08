674054eb9879eece9a7d0b279c9720df8073cb01
Willroy Myles <willroymyles@yahoo.com>
2020-06-08 03:30:36 -0500

    Added react app as submodule

--- Controller.js
+++ Controller.js
@@ -2,14 +2,14 @@ module.exports = (app) => {
 	const express = require("express")
 	const path = require("path")
 
-	app.use(express.static(path.join(__dirname, "wrg-autoparts/build")))
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname + "/wrg-autoparts/build/index.html"))
-	})
+	app.use(express.static(path.join(__dirname, "wrg/build")))
 
 	app.get("/api", (req, res) => {
 		res.json("hi")
+	})
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname + "/wrg/build/index.html"))
 	})
 
 	console.log("started")
