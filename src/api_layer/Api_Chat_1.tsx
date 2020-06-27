bb875d7fad1a38bf00ccb795c8b7d10211df944e
Willroy Myles <willroymyles@yahoo.com>
2020-06-27 03:05:33 -0500

    grrr

--- src/api_layer/Api_Chat_1.tsx
+++ src/api_layer/Api_Chat_1.tsx
@@ -5,7 +5,7 @@ import moment from "moment"
 import dotenv from "dotenv"
 
 export const sendMessage = (payload: any) => {
-	const url = "https://hooks.slack.com/services/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"
+	const url = "https://hooks.slack.com/services/T016JB9PBC0/B01646ZGEJH/Tdas2upa3eIUEqFZfZKzQYi" + "V"
 	const botUserToken = "xoxb-1222383793408-1209761931043-CdSces0VvjyV53LMZWQSzCHq"
 	const feedbackId = "C016BEEPF52"
 	return new Promise((resolve, reject) => {
