7a6264bd161d958d6579723cc99ec60c817e606b
Willroy Myles <willroymyles@yahoo.com>
2020-07-05 01:45:28 -0500

    Added git add subtree command to scripts
    Added git add subtree command to scripts
    
    Changed ref name for subtree
    
    Changed ref name for subtree
    
    Changed ref name for subtree

--- wrg/src/components/helpers/Styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import {Input} from "antd"
-
-export const Winput = styled(Input)`
-	padding: 10;
-	// background-color: red;
-	border-radius: 7px;
-	margin: 5px;
-`
